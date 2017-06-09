791859a5a012d09c46c5d5ba569bee357d8357c1
John Asmuth <jasmuth@google.com>
2017-06-09 12:28:19 -0400

    Coord establishes topic and its own subscription based on the build ID.

--- coord/README.md
+++ coord/README.md
@@ -5,3 +5,10 @@ The `coord` container image coordinates a `flargo` workflow.
 It listens to a Cloud Pub/Sub topic to keep track of each `flargo` execution, such that only the id of the build running the coord is needed in order to get a view of the entire workflow.
 
 For each execution that begins, ends, is retried or skipped, the `coord` build step will write a log message that can be consulted by the `flargo` tool later.
+
+For this container image to run as a Container Builder step, the builder service account needs following permissions:
+ - pubsub.subscriptions.consume
+ - pubsub.subscriptions.create
+ - pubsub.topics.attachSubscription
+ - pubsub.topics.create
+ - pubsub.topics.publish
--- coord/coord.go
+++ coord/coord.go
@@ -19,8 +19,11 @@ import (
 	"log"
 	"os"
 	"strings"
+	"time"
 
+	"cloud.google.com/go/compute/metadata"
 	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
 	pubsub_v1 "google.golang.org/api/pubsub/v1"
 )
@@ -31,30 +34,57 @@ The coord program listens for certain kinds of messages on a pubsub subscription
 by flargo.
 */
 
+func init() {
+	// Because the metadata package can't figure this out itself.
+	os.Setenv("GCE_METADATA_HOST", "metadata.google.internal")
+}
+
 func main() {
 	ctx := context.Background()
 
 	if len(os.Args) != 2 {
-		fmt.Println("Usage: %s SUBSCRIPTION", os.Args[0])
+		fmt.Println("Usage: %s ID", os.Args[0])
 	}
-	subs := os.Args[1]
 
-	client, err := google.DefaultClient(ctx, "https://www.googleapis.com/auth/cloud-platform")
+	buildID := os.Args[1]
+	projectID, err := metadata.ProjectID()
 	if err != nil {
-		log.Fatalf("Could not create authed http client: %v", err)
+		log.Fatalf("Could not get project ID")
 	}
+
+	ts := google.ComputeTokenSource("")
+
+	client := oauth2.NewClient(ctx, ts)
 
 	pubsub, err := pubsub_v1.New(client)
 	if err != nil {
 		log.Fatalf("Could not create pubsub client: %v", err)
 	}
 
+	tname := fmt.Sprintf("projects/%s/topics/workflow-%s", projectID, buildID)
+
+	if _, err := pubsub.Projects.Topics.Create(tname, &pubsub_v1.Topic{
+		Name: buildID,
+	}).Do(); err != nil {
+		log.Fatalf("Could not create topic: %v", err)
+	}
+
+	sname := fmt.Sprintf("projects/%s/subscriptions/coord-%s", projectID, buildID)
+
+	if _, err := pubsub.Projects.Subscriptions.Create(sname, &pubsub_v1.Subscription{
+		Name:  "coord-" + buildID,
+		Topic: tname,
+	}).Do(); err != nil {
+		log.Fatalf("Could not create subscription: %v", err)
+	}
+
 	for {
-		resp, err := pubsub.Projects.Subscriptions.Pull(subs, &pubsub_v1.PullRequest{
+		time.Sleep(1 * time.Second)
+		resp, err := pubsub.Projects.Subscriptions.Pull(sname, &pubsub_v1.PullRequest{
 			MaxMessages: 1,
 		}).Do()
 		if err != nil {
-			log.Printf("Error pulling subscription %q: %v", subs, err)
+			log.Printf("Error pulling subscription %q: %v", sname, err)
 			continue
 		}
 		for _, rmsg := range resp.ReceivedMessages {
@@ -64,7 +94,7 @@ func main() {
 			io.Copy(os.Stdout, base64.NewDecoder(base64.StdEncoding, strings.NewReader(msg)))
 			fmt.Println()
 
-			if _, err := pubsub.Projects.Subscriptions.Acknowledge(subs, &pubsub_v1.AcknowledgeRequest{
+			if _, err := pubsub.Projects.Subscriptions.Acknowledge(sname, &pubsub_v1.AcknowledgeRequest{
 				AckIds: []string{rmsg.AckId},
 			}).Do(); err != nil {
 				log.Printf("Failed to ack message %q: %v", rmsg.AckId, err)
